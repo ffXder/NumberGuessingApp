bcf9e8e835a4ec93cd16d53859392be8667cfa32
ffXder <fjfajardo0624@gmail.com>
2025-02-14 20:05:11 +0800

    -fixes errors

--- NumberGuessingApp/Form1.Designer.cs
+++ NumberGuessingApp/Form1.Designer.cs
@@ -28,43 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            play_button = new Button();
             textBox1 = new TextBox();
-            button2 = new Button();
+            exit_button = new Button();
             pictureBox1 = new PictureBox();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
-            // button1
+            // play_button
             // 
-            button1.Location = new Point(289, 307);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 0;
-            button1.Text = "Play";
-            button1.UseVisualStyleBackColor = true;
+            play_button.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            play_button.BackColor = Color.Bisque;
+            play_button.BackgroundImageLayout = ImageLayout.None;
+            play_button.FlatStyle = FlatStyle.Popup;
+            play_button.Font = new Font("Cooper Black", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            play_button.ForeColor = SystemColors.ControlDarkDark;
+            play_button.Location = new Point(123, 244);
+            play_button.Name = "play_button";
+            play_button.Size = new Size(89, 43);
+            play_button.TabIndex = 0;
+            play_button.Text = "Guess";
+            play_button.UseVisualStyleBackColor = false;
+            play_button.Click += play_button_Click;
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(315, 234);
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Font = new Font("Arial Rounded MT Bold", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(181, 183);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(145, 23);
+            textBox1.Size = new Size(113, 32);
             textBox1.TabIndex = 1;
+            textBox1.TextAlign = HorizontalAlignment.Center;
             // 
-            // button2
+            // exit_button
             // 
-            button2.Location = new Point(403, 307);
-            button2.Name = "button2";
-            button2.Size = new Size(75, 23);
-            button2.TabIndex = 3;
-            button2.Text = "Exit";
-            button2.UseVisualStyleBackColor = true;
+            exit_button.BackColor = Color.Silver;
+            exit_button.FlatStyle = FlatStyle.Popup;
+            exit_button.Font = new Font("Cooper Black", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            exit_button.ForeColor = Color.DimGray;
+            exit_button.Location = new Point(261, 244);
+            exit_button.Name = "exit_button";
+            exit_button.Size = new Size(90, 43);
+            exit_button.TabIndex = 3;
+            exit_button.Text = "Exit";
+            exit_button.UseVisualStyleBackColor = false;
+            exit_button.Click += exit_button_Click;
             // 
             // pictureBox1
             // 
-            pictureBox1.Location = new Point(411, 132);
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(-5, -154);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(100, 50);
+            pictureBox1.Size = new Size(523, 282);
             pictureBox1.TabIndex = 4;
             pictureBox1.TabStop = false;
             // 
@@ -74,13 +91,15 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.LemonChiffon;
             BackgroundImageLayout = ImageLayout.None;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(471, 403);
             Controls.Add(pictureBox1);
-            Controls.Add(button2);
+            Controls.Add(exit_button);
             Controls.Add(textBox1);
-            Controls.Add(button1);
+            Controls.Add(play_button);
             FormBorderStyle = FormBorderStyle.FixedSingle;
             Location = new Point(20, 20);
+            MaximizeBox = false;
+            MinimizeBox = false;
             Name = "Form1";
             Text = "Number Guessing";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
@@ -90,9 +109,9 @@
 
         #endregion
 
-        private Button button1;
+        private Button play_button;
         private TextBox textBox1;
-        private Button button2;
-        private PictureBox pictureBox1;
+        private Button exit_button;
+        public PictureBox pictureBox1;
     }
 }
--- NumberGuessingApp/Form1.cs
+++ NumberGuessingApp/Form1.cs
@@ -1,3 +1,5 @@
+using System.Text.RegularExpressions;
+
 namespace NumberGuessingApp
 {
     public partial class Form1 : Form
@@ -6,6 +8,37 @@ namespace NumberGuessingApp
         {
             InitializeComponent();
         }
-
+        //Guess Button
+        private void play_button_Click(object sender, EventArgs e)
+        {
+            Random random = new Random(); //using random
+            int guessNumber = random.Next(1, 10); //guess the number up to 1 to 10
+            String showNumber = Convert.ToString(guessNumber);
+            Match matchNumber = Regex.Match(textBox1.Text, @"\d+"); //checks if the user input are all the digits using regex
+            if (matchNumber.Success)
+            {
+                if (textBox1.Text == showNumber)
+                {
+                    MessageBox.Show(" You guessed it right!" + "\n The correct answer is " + showNumber);
+                }
+                else
+                {
+                    MessageBox.Show(" Wrong! try again!" + "\n The correct answer is " + showNumber);
+                }
+            } else {
+                DialogResult dr = MessageBox.Show("Please input a number", "Wrong Input", MessageBoxButtons.OK ,MessageBoxIcon.Warning);
+            }
+            textBox1.Clear(); //clears the textbox after pressing the button
+        }
+        //Exit Button
+        private void exit_button_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("Do you want to exit?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dr == DialogResult.Yes)
+            {
+                MessageBox.Show("Thank you for using our game :>");
+                this.Close();
+            }
+        }
     }
 }
